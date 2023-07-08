117f1cd87fa9384d060ab25fee9d65cfd88b2a5b
CNYuYang <744727849@qq.com>
2023-07-08 22:06:46 +0800

    kafka producer

--- kafka/src/run/yuyang/kafka/ProducerDemo.java
+++ kafka/src/run/yuyang/kafka/ProducerDemo.java
@@ -1,9 +1,33 @@
 package run.yuyang.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
+        sendOneSync();
+    }
 
+
+    private static void sendOneSync() {
+        Properties kafkaProps = new Properties();
+        kafkaProps.put("bootstrap.servers", "192.168.3.14:9092");
+        kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(kafkaProps);
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first", "Precision Products", "France");
+
+        try {
+            producer.send(record);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        producer.close();
     }
 
 }
